dad112fd983fdd1997a6ee77860cda91bbf752de
Keiya Ishiguro <keyspire@gmail.com>
2024-11-03 05:30:50 +0900

    add Student-t-test for simple implementation to test

--- sandbox/_main.cpp
+++ sandbox/_main.cpp
@@ -6,11 +6,21 @@
 #include <random>
 #include <vector>
 
-feed_normal_rv::feed_normal_rv(uint64_t seed)
+auto table_student_t_95p = std::vector<double>{
+    12.7062047362, 4.30265272975, 3.18244630528, 2.77644510520, 2.57058183564,
+    2.44691185114, 2.36462425159, 2.30600413520, 2.26215716280, 2.22813885199,
+    2.20098516009, 2.17881282967, 2.16036865646, 2.14478668792, 2.13144954556,
+    2.11990529922, 2.10981557783, 2.10092204024, 2.09302405441, 2.08596344727,
+    2.07961384473, 2.07387306790, 2.06865761042, 2.06389856163, 2.05953855275,
+    2.05552943864, 2.05183051648, 2.04840714180, 2.04522964213, 2.04227245630};
+
+analyze::feed_normal_rv::feed_normal_rv(uint64_t seed)
     : _engine{std::mt19937_64{seed}},
       _dist{std::normal_distribution<>{0., 1.}} {}
 
-double feed_normal_rv::get_single_normal_rv() { return _dist(_engine); }
+double analyze::feed_normal_rv::get_single_normal_rv() {
+  return _dist(_engine);
+}
 
 double update_bs_process(double log_current_val, const double dt,
                          const double vol, const double rfr,
@@ -57,15 +67,43 @@ double expectation_fxrate_at_maturity(const double term, const double spot,
   return spot * std::exp((rate_base - rate_foreign) * term);
 }
 
+std::vector<analyze::result_t> analyze::get_95p_confidence_interval_t_test(
+    const std::vector<result_t> divided_results) {
+  auto _size = divided_results.size();
+  auto _variance_vec = analyze::result_t{};
+  for (auto i = 0; i < _size; ++i) {
+    if (divided_results.at(i).size() > 30 || divided_results.at(i).size() < 1) {
+      throw std::logic_error(
+          "we have not implemented t-distribution value yet.");
+    }
+    _variance_vec.push_back(analyze::variance(divided_results.at(i)));
+  }
+
+  auto result = std::vector<analyze::result_t>{};
+  for (auto i = 0; i < _size; ++i) {
+    auto _mean = std::reduce(divided_results.at(i).begin(),
+                             divided_results.at(i).end(), 0.0);
+    auto _n = divided_results.at(i).size();
+    _mean /= (static_cast<double>(_n));
+    auto _variance = _variance_vec.at(i);
+    double _r_minus;
+    double _r_plus;
+    _r_minus =
+        _mean - table_student_t_95p.at(_n - 1) * std::sqrt(_variance / _n);
+    _r_plus =
+        _mean + table_student_t_95p.at(_n - 1) * std::sqrt(_variance / _n);
+    result.push_back(analyze::result_t{_r_minus, _r_plus});
+  }
+
+  return result;
+}
+
 // åpè≥ÇÕÇµÇ»Ç¢ÇŸÇ§Ç™Ç¢Ç¢ÇÁÇµÇ¢
 int main() {
-  auto random_0{feed_normal_rv{0}};
-  auto random_5489{feed_normal_rv{5489}};
+  auto random_0{analyze::feed_normal_rv{428}};
+  auto random_5489{analyze::feed_normal_rv{5489}};
 
-  constexpr double spot = 100;
-  constexpr uint64_t num_runs = 500000;
-
-  const double log_spot = std::log(spot);
+  constexpr uint64_t num_runs = 50000;
 
   double log_current_val_0;
   double log_current_val_5489;
@@ -73,13 +111,15 @@ int main() {
   double log_current_val_5489_neg;
   double fx_rate;
 
+  constexpr double spot = 100.0;
+  const double log_spot = std::log(spot);
   constexpr double dt = 1.;
-  constexpr double vol = 0.25;
-  constexpr double numeraire_rate = 0.01;
-  constexpr double rate_foreign = 0.05;
-  constexpr double rate_base = 0.01;
-  constexpr double rfr = -0.04;
-  constexpr double strike = 100;
+  constexpr double vol = 0.2;
+  constexpr double rate_foreign = 0.;
+  constexpr double rate_base = 0.;
+  const double numeraire_rate = rate_base;
+  constexpr double rfr = rate_base - rate_foreign;
+  constexpr double strike = 100.0;
 
   double payoff_0{0.};
   double payoff_5489{0.};
@@ -128,8 +168,8 @@ int main() {
         coeff * std::exp(log_current_val_5489) -
         coeff * std::exp(log_current_val_5489_neg);
 
-    /* vec_payoff_0.push_back(payoff_0);
-     vec_payoff_5489.push_back(payoff_5489);*/
+    vec_payoff_0.push_back(payoff_0);
+    vec_payoff_5489.push_back(payoff_5489);
   }
 
   const double discount_f = std::exp(-numeraire_rate * 1.0);
@@ -140,15 +180,38 @@ int main() {
   double analytic_fxrate_expect =
       expectation_fxrate_at_maturity(dt, spot, rate_foreign, rate_base);
 
+  analyze::result_t vec_payoff_0_mod;
+  analyze::result_t vec_payoff_5489_mod;
+  uint64_t _sum = 1;
+  for (auto i = 0; i < num_runs; ++i) {
+    vec_payoff_0_mod.push_back(vec_payoff_0.at(i) * discount_f / (2. * _sum));
+    vec_payoff_5489_mod.push_back(vec_payoff_5489.at(i) * discount_f /
+                                  (2. * _sum));
+    _sum += 1;
+  }
+
+  auto error_bar_0 = analyze::get_95p_confidence_interval_t_test(
+      analyze::get_partition(vec_payoff_0_mod, 30));
+  auto error_bar_5489 = analyze::get_95p_confidence_interval_t_test(
+      analyze::get_partition(vec_payoff_5489_mod, 30));
+
+  // console out
   std::cout << "base_rate: " << rate_base << ", foreign_rate: " << rate_foreign
-            << ", ATM spot = 100, vol: " << vol << ", term (y):" << dt
-            << std::endl;
+            << ", spot = " << spot << ", strike = " << strike
+            << ", vol: " << vol << ", term (y):" << dt << std::endl;
   std::cout << "mc: negative_corr method, num_run: " << num_runs << std::endl
             << "seed = 0 result: " << payoff_0 * discount_f / (2. * num_runs)
+            << std::endl
+            << "95% interval lower side: " << error_bar_0.back().at(0)
+            << ", 95% interval higher side: " << error_bar_0.back().at(1)
             << std::endl
 
             << "seed = 5489 result: "
             << payoff_5489 * discount_f / (2. * num_runs) << std::endl
+            << "95% interval lower side: " << error_bar_5489.back().at(0)
+            << ", 95% interval higher side: " << error_bar_5489.back().at(1)
+            << std::endl
+
             << "mc: negative_corr + control_val method, num_run: " << num_runs
             << std::endl
             << "seed = 0 result: "
--- sandbox/_main.h
+++ sandbox/_main.h
@@ -1,11 +1,65 @@
 #pragma once
+#include <algorithm>
+#include <numeric>
 #include <random>
+#include <vector>
+
+namespace analyze {
+using result_t = std::vector<double>;
+
+template <typename T>
+std::vector<std::vector<T>> get_partition(const std::vector<T>& vec,
+                                          const uint64_t cell_size) {
+  const size_t length = vec.size();
+  auto size = length / cell_size;
+  auto is_zero_remainder = (length % cell_size == 0) ? 0 : 1;
+ std::vector<std::vector<T>> div_vec;
+  //div_vec.resize(size + is_zero_remainder);
+  for (auto i = 0; i < size + is_zero_remainder; ++i) {
+    auto _temp_vec = std::vector<T>{};
+    if (i == size) {
+      for (auto j = 0; j < length % cell_size; ++j) {
+        _temp_vec.push_back(vec.at(cell_size * i + j));
+      }
+    } else if (i < size) {
+      for (auto j = 0; j < cell_size; ++j) {
+        _temp_vec.push_back(vec.at(cell_size * i + j));
+      }
+    }
+    div_vec.push_back(_temp_vec);
+  }
+  return div_vec;
+}
+
+
+template <typename T>
+T variance(const std::vector<T>& vec) {
+  const size_t sz = vec.size();
+  if (sz <= 1) {
+    return 0.0;
+  }
+
+  // Calculate the mean
+  const T mean = std::accumulate(vec.begin(), vec.end(), 0.0) / sz;
+
+  // Now calculate the variance
+  auto variance_func = [&mean, &sz](T accumulator, const T& val) {
+    return accumulator + ((val - mean) * (val - mean) / (sz - 1));
+  };
+
+  return std::accumulate(vec.begin(), vec.end(), 0.0, variance_func);
+}
+
+std::vector<result_t> get_95p_confidence_interval_t_test(const std::vector<result_t> divided_results);
 
 class feed_normal_rv {
  public:
   feed_normal_rv(const uint64_t seed);
   double get_single_normal_rv();
+
  private:
   std::mt19937_64 _engine;
   std::normal_distribution<> _dist;
 };
+
+}  // namespace main
